6de73f26f7d4b19110a2c00f769323d2121ed09c
kultyapka3 <igor200322@gmail.com>
2026-05-06 14:56:52 +0300

    fixed tests/get_post_by_id.py

--- tests/test_get_post_by_id.py
+++ tests/test_get_post_by_id.py
@@ -18,17 +18,16 @@ def test_get_post_by_id(
 ) -> None:
     response = api_client.get_post(post_id=test_post)
     parsed: ParsedResponse = parse_api_response(response)
-    post_id: int = parsed.body["id"]
 
     assert (
         parsed.status_code == 200
     ), f"Ожидался статус 200 OK, но получен {parsed.status_code}"
     assert (
-        parsed.body["id"] == post_id
-    ), f"ID в ответе ({parsed.body["id"]}) не совпадает с созданным ({post_id})"
+        parsed.body["id"] == test_post
+    ), f"ID в ответе ({parsed.body["id"]}) не совпадает с созданным ({test_post})"
 
-    result = db_client.get_post_by_id(post_id)
+    result = db_client.get_post_by_id(test_post)
 
     assert (
         result is not None and result["post_title"] == "Test Title"
-    ), f"Пост с ID {post_id} не найден в БД или имеет неверные данные"
+    ), f"Пост с ID {test_post} не найден в БД или имеет неверные данные"
